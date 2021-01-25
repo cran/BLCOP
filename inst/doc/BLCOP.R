f4048bc3d65725b3c7c9b64937d526e10f64e5d9
Joe Russell <jrussell@mango-solutions.com>
2021-01-25 22:00:02 +0000

    version 0.3.3

--- inst/doc/BLCOP.R
+++ inst/doc/BLCOP.R
@@ -1,78 +1,58 @@
-### R code from vignette source 'BLCOP.Rnw'
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  echo = TRUE,
+  fig.align = "center", 
+  fig.width = 6, 
+  fig.height = 6)
 
-###################################################
-### code chunk number 1: BLCOP.Rnw:76-79
-###################################################
+## ---- message = FALSE, warning = FALSE, print = FALSE-------------------------
 library(fPortfolio)
 library(BLCOP)
 library(mnormt)
 
-
-###################################################
-### code chunk number 2: BLCOP.Rnw:81-85
-###################################################
+## ---- print = FALSE-----------------------------------------------------------
 pickMatrix <- matrix(c(1/2, -1, 1/2, rep(0, 3)), nrow = 1, ncol = 6 )
-views <- BLViews(P = pickMatrix, q = 0.06,confidences =  100, 
+views <- BLViews(P = pickMatrix, q = 0.06,confidences =  100,
 assetNames = colnames(monthlyReturns))
 views
 
-
-###################################################
-### code chunk number 3: BLCOP.Rnw:90-92
-###################################################
+## ---- print = FALSE-----------------------------------------------------------
 priorMeans <- rep(0, 6)
 priorVarcov <- MASS::cov.mve(monthlyReturns)$cov
 
-
-###################################################
-### code chunk number 4: BLCOP.Rnw:100-102
-###################################################
-marketPosterior <- posteriorEst(views = views, sigma = priorVarcov, 
+## ---- print = TRUE------------------------------------------------------------
+marketPosterior <- posteriorEst(views = views, sigma = priorVarcov,
  mu = priorMeans, tau = 1/2)
+marketPosterior
 
-
-###################################################
-### code chunk number 5: BLCOP.Rnw:107-111
-###################################################
+## ---- print = FALSE-----------------------------------------------------------
 finViews <- matrix(ncol = 4, nrow = 1, dimnames = list(NULL, c("C","JPM","BAC","MS")))
 finViews[,1:4] <- rep(1/4,4)
 views <- addBLViews(finViews, 0.15, 90, views)
 views
 
-
-###################################################
-### code chunk number 6: BLCOP.Rnw:118-120
-###################################################
-marketPosterior <- BLPosterior(as.matrix(monthlyReturns), views, tau = 1/2, 
+## ---- print = TRUE------------------------------------------------------------
+marketPosterior <- BLPosterior(as.matrix(monthlyReturns), views, tau = 1/2,
 	marketIndex = as.matrix(sp500Returns),riskFree = as.matrix(US13wTB))
+marketPosterior
 
-
-###################################################
-### code chunk number 7: BLCOP.Rnw:136-140
-###################################################
+## ---- print = TRUE, fig = TRUE, fig.height = 3--------------------------------
 optPorts <- optimalPortfolios.fPort(marketPosterior, optimizer = "tangencyPortfolio")
-par(mfcol = c(2, 1))
+optPorts
 weightsPie(optPorts$priorOptimPortfolio)
 weightsPie(optPorts$posteriorOptimPortfolio)
 
-
-###################################################
-### code chunk number 8: BLCOP.Rnw:146-149
-###################################################
-optPorts2 <- optimalPortfolios.fPort(marketPosterior, 
+## ---- echo=TRUE, print=FALSE, fig=FALSE---------------------------------------
+optPorts2 <- optimalPortfolios.fPort(marketPosterior,
 		constraints = "minW[1:6]=0.1", optimizer = "minriskPortfolio")
 optPorts2
 
-
-###################################################
-### code chunk number 9: BLCOP.Rnw:155-156
-###################################################
+## ---- echo=TRUE, print=FALSE, fig=TRUE----------------------------------------
 densityPlots(marketPosterior, assetsSel = "JPM")
 
-
-###################################################
-### code chunk number 10: BLCOP.Rnw:207-214
-###################################################
+## ---- echo=TRUE, print=FALSE--------------------------------------------------
 dispersion <- c(.376,.253,.360,.333,.360,.600,.397,.396,.578,.775) / 1000
 sigma <- BLCOP:::.symmetricMatrix(dispersion, dim = 4)
 caps <- rep(1/4, 4)
@@ -81,32 +61,19 @@ dim(mu) <- NULL
 marketDistribution <- mvdistribution("mt", mean = mu, S = sigma, df = 5 )
 class(marketDistribution)
 
-
-###################################################
-### code chunk number 11: BLCOP.Rnw:222-228
-###################################################
-pick <- matrix(0, ncol = 4, nrow = 1, 
-	dimnames = list(NULL, c("SP", "FTSE", "CAC", "DAX")))
+## ---- echo=TRUE, print=FALSE--------------------------------------------------
+pick <- matrix(0, ncol = 4, nrow = 1, dimnames = list(NULL, c("SP", "FTSE", "CAC", "DAX")))
 pick[1,"DAX"] <- 1
 viewDist <- list(distribution("unif", min = -0.02, max = 0))
-views <- COPViews(pick, viewDist = viewDist, confidences = 0.2, 
-	assetNames = c("SP", "FTSE", "CAC", "DAX"))
+views <- COPViews(pick, viewDist = viewDist, confidences = 0.2, assetNames = c("SP", "FTSE", "CAC", "DAX"))
 
-
-###################################################
-### code chunk number 12: BLCOP.Rnw:233-238
-###################################################
+## ---- echo=TRUE, print=FALSE--------------------------------------------------
 newPick <- matrix(0, 1, 2)
 dimnames(newPick) <- list(NULL, c("SP", "FTSE"))
 newPick[1,] <- c(1, -1) # add a relative view
-views <- addCOPViews(newPick, 
-	list(distribution("norm", mean = 0.05, sd = 0.02)), 0.5, views)
+views <- addCOPViews(newPick, list(distribution("norm", mean = 0.05, sd = 0.02)), 0.5, views)
 
-
-###################################################
-### code chunk number 13: BLCOP.Rnw:243-245
-###################################################
+## ---- echo=TRUE, print=FALSE, fig=TRUE----------------------------------------
 marketPosterior <- COPPosterior(marketDistribution, views, numSimulations = 50000)
 densityPlots(marketPosterior, assetsSel = 4)
-
 
